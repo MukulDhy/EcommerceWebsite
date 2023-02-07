ee618aecb857ce9e32a02a8d0a5be2cb1a9c1dcb
Mukul Dahiya <98389203+MukulDhy@users.noreply.github.com>
2023-02-07 19:16:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectionDb();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://ecommercewebsitef.onrender.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
